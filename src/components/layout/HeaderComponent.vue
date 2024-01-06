4e1dbaf065d05d114dfab2ab4f323203d26182d1
bekCoder1997 <84372506+bekCoder1997@users.noreply.github.com>
2024-01-06 14:10:35 +0500

    local chnages

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <MobileHeaderComponent/>
+  <!-- Header -->
+    <div class="edrea_tm_header">
+        <div class="header_inner">
+            <div class="logo">
+                <a href="#" @click="navFuntions.activeSection('home')"
+                ><img src="img/logo/logo.png" alt=""
+                /></a>
+            </div>
+            <div class="menu">
+                <Menu/>
+            </div>
+            <div class="logo">
+                <a href="#" @click.prevent="handleSwitchLanguage('uz')"> <img src="/img/flag/uz.png" alt=""/></a>
+                <a href="#" @click.prevent="handleSwitchLanguage('ru')"> <img src="/img/flag/ru.png" alt=""/></a>
+                <a href="#" @click.prevent="handleSwitchLanguage('en')"> <img src="/img/flag/en.png" alt=""/></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {navFuntions} from "@/navFuntions";
+import Menu from "./Menu.vue";
+import MobileHeaderComponent from "./MobileHeaderComponent.vue";
+
+export default {
+    name: `HeaderComponent`,
+    components: {MobileHeaderComponent, Menu},
+    data() {
+        return {
+            navFuntions,
+        };
+    },
+};
+</script>
